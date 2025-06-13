6954f4f000455838b97e35984883a8075b2bd401
vivharper <viv.harper.13@gmail.com>
2025-06-13 15:04:48 -0400

    feat(site): add resume and footer links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Home from "./home";
+import styles from "../styles/page/home.module.scss";
+import About from "./about";
+import Work from "./work";
 
 export default function Page() {
   return (
-    <div>
-      <Home />
+    <div className={styles.content}>
+      <About />
+      <Work />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,41 @@
+import styles from "../styles/component/footer.module.scss";
+
 const Footer = (props: any) => {
-  return <footer>{props.children}</footer>;
+  return (
+    <footer className={styles.root}>
+      <div className={styles.content}>
+        <ul>
+          <li>
+            <a
+              href="/Vivian_Dickerson_latest.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/vivharper"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/vivian-dickerson/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
